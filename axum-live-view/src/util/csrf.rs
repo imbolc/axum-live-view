6565b605b8bef570485e1fd224159c233dab4b59
imbolc <me@imbolc.name>
2023-02-13 15:44:17 +0600

    Add `SameSite=Lax` attribute to the cookie

--- axum-live-view/src/util/csrf.rs
+++ axum-live-view/src/util/csrf.rs
@@ -16,7 +16,13 @@ const URI_NAME: &str = "csrf=";
 /// Sets a cookie with a random CSRF token
 pub(crate) fn set_cookie(headers: &mut HeaderMap) {
     let token = random_token();
-    if let Ok(val) = format!("{COOKIE_NAME}{token}").parse() {
+    // SameSite=Lax means that the cookie is not sent on cross-site requests,
+    // such as on requests to load images or frames, but is sent when a user
+    // is navigating to the origin site from an external site (for example,
+    // when following a link). This is the default behavior if the SameSite
+    // attribute is not specified.
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie
+    if let Ok(val) = format!("{COOKIE_NAME}{token}; SameSite=Lax").parse() {
         headers.insert(SET_COOKIE, val);
     }
 }
